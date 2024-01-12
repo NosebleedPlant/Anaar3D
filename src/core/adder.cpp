5423c2df263f0bea7c5d233df29101a9b54db8b9
Yarrow <42461443+NosebleedPlant@users.noreply.github.com>
2024-01-12 02:04:46 -0700

    testing files set 1
    used to figure out how cmake can link between internal libraries

---
+++ src/core/adder.cpp
@@ -0,0 +1,5 @@
+#include "../../include/core/adder.hpp"
+
+int adder::add(int a, int b){
+    return a+b;
+}
---
+++ src/core/subtractor.cpp
@@ -0,0 +1,5 @@
+#include "../../include/core/subtractor.hpp"
+
+int subtractor::subtract(int a, int b){
+    return a-b;
+}
