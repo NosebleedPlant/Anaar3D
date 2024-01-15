9713a417f145b0a82cfd5b70fae355700d9c1cb5
Yarrow <42461443+NosebleedPlant@users.noreply.github.com>
2024-01-15 16:34:52 -0700

    set up two fucntions for context
    wsl ubuntu is stuck at opengl 4.2 i will need to upgrade this before compute shaders are possible

--- console/main.cpp
+++ console/main.cpp
@@ -1,4 +1,8 @@
+#include <Anaar/test.hpp>
+
 int main(void)
 {
+    anaar_framework::CreateAFContext_IMGUI();
     std::cout<<"Helloworld"<<std::endl;
+    return 1;
 }
---
+++ framework/include/Anaar/test.hpp
@@ -0,0 +1,6 @@
+namespace anaar_framework{
+    
+
+    int CreateAFContext_Raw ();
+    int CreateAFContext_IMGUI ();
+}
