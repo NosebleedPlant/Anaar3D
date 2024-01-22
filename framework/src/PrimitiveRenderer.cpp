9885492859debf2f72ddefaa6322b92d8d9974ba
NosebleedPlant <yalmazabdullah.main@gmail.com>
2024-01-22 01:22:46 -0700

    lesson 3

--- framework/src/PrimitiveRenderer.cpp
+++ framework/src/PrimitiveRenderer.cpp
@@ -7,6 +7,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
 
 #include "PrimitiveRenderer.hpp"
 #include "Shader.hpp"
@@ -51,15 +52,16 @@ int anaar_framework::CreateAFContext_Raw()
     // "copy files cmake"
     // custom command in cmake seems to be the way to go
     Shader ourShader(
-        "/home/yarrow/Anaar/framework/resources/shaders/test.vs", 
-        "/home/yarrow/Anaar/framework/resources/shaders/test.fs"
+        "/home/yarrow/Anaar/framework/resources/shaders/texture.vs", 
+        "/home/yarrow/Anaar/framework/resources/shaders/texture.fs"
     );
 
     float vertices[] = {
-     0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,// top right
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,// bottom right
-    -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 0.0f,// bottom left
-    -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f // top left 
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
     };
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,   // first triangle
@@ -67,10 +69,10 @@ int anaar_framework::CreateAFContext_Raw()
     };
 
     unsigned int VAO;                                                             
-    glGenVertexArrays(1, &VAO);                                                         // Make VAO
+    glGenVertexArrays(1, &VAO); // Make VAO
 
     unsigned int VBO;                                                                
-    glGenBuffers(1, &VBO);                                                              // Make VBO
+    glGenBuffers(1, &VBO);      // Make VBO
     
     //First Triangle
     glBindVertexArray(VAO);                                                         
@@ -78,19 +80,60 @@ int anaar_framework::CreateAFContext_Raw()
     
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);    
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);     
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);   
     glEnableVertexAttribArray(0);                       
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);                                                                          
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);                                                                            
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+    unsigned int textures[2];
+    glGenTextures(2, textures);  
+
+    glBindTexture(GL_TEXTURE_2D, textures[0]); 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);  
+    unsigned char *data = stbi_load("/home/yarrow/Anaar/framework/resources/textures/container.jpg", &width, &height, &nrChannels, 0);
+    if (data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else{
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glBindTexture(GL_TEXTURE_2D, textures[1]); 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    data = stbi_load("/home/yarrow/Anaar/framework/resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else{
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);                                                   // Unbind VBO
     glBindVertexArray(0);                                                               // Unbind VAO
 
+    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -98,9 +141,12 @@ int anaar_framework::CreateAFContext_Raw()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
+        
         ourShader.use();
-        float timeValue = glfwGetTime();
-        ourShader.setFloat("offset", sin(timeValue));
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -110,6 +156,7 @@ int anaar_framework::CreateAFContext_Raw()
     
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
