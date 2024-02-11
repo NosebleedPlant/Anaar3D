70662104a354ed3a0330550ab09538301a78abe4
Yarrow <42461443+NosebleedPlant@users.noreply.github.com>
2024-02-10 22:45:10 -0700

    modification to vbo

--- include/vertexBuffer.hpp
+++ include/vertexBuffer.hpp
@@ -6,9 +6,8 @@ class VertexBuffer{
         GLuint m_renderID;
     public:
         VertexBuffer(GLsizeiptr size, GLfloat* vertices);
-        ~VertexBuffer();
-        void bindBuffer();
-        void unbindBuffer();
-        void deleteBuffer();
-        GLuint get_ID();
+        void Bind();
+        void Unbind();
+        void Delete();
+        GLuint getID();
 };
--- src/vertexBuffer.cpp
+++ src/vertexBuffer.cpp
@@ -3,25 +3,21 @@
 VertexBuffer::VertexBuffer(GLsizeiptr size, GLfloat* vertices){
     glGenBuffers(1,&m_renderID);
     glBindBuffer(GL_ARRAY_BUFFER,m_renderID);
-    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);  
+    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW); 
 }
 
-VertexBuffer::~VertexBuffer(){
-    glDeleteBuffers(1,&m_renderID);
-}
-
-void VertexBuffer::bindBuffer(){
+void VertexBuffer::Bind(){
     glBindBuffer(GL_ARRAY_BUFFER,m_renderID);
 }
 
-void VertexBuffer::unbindBuffer(){
+void VertexBuffer::Unbind(){
     glBindBuffer(GL_ARRAY_BUFFER,0);
 }
 
-void VertexBuffer::deleteBuffer(){
+void VertexBuffer::Delete(){
     glDeleteBuffers(1,&m_renderID);
 }
 
-GLuint VertexBuffer::get_ID(){
+GLuint VertexBuffer::getID(){
     return m_renderID;
 }
